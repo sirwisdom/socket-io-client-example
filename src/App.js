453bf51e677c84423901104374427018bbfd246b
Akpomeyoma Wisdom <wakpomeyoma@gmail.com>
2022-01-07 12:43:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
 function App() {
+  const server = "http://localhost:2020/";
+
+  const socket = io(server);
+
+  useEffect(() => {
+    socket.on("connection", () => {
+      console.log("Connected to Server Successfully");
+    });
+  }, []);
+  console.log("S oooooo", socket);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello, I am building a WebSocket Application with React.js and Express{" "}
+      </h1>
+
+      <div>
+        <h2>
+          Current Price: &nbsp; <span>50000</span>
+        </h2>
+      </div>
     </div>
   );
 }
